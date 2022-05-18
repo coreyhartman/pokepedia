18134619a6994985b0ca0078a65cb6fdc6e771d3
Corey Hartman <corey.hartman1420@gmail.com>
2022-05-17 21:19:42 -0400

    Changed border radius on pokemon grid card.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -8,7 +8,7 @@ export default function PokemonCard({ name }) {
     useEffect(() => { getPokemonCardInfo(name).then(x => setCardInfo(x)) }, []);
 
     return (
-        <div className="p-4 flex flex-col items-center space-y-2 bg-gray-50 rounded">
+        <div className="p-4 flex flex-col items-center space-y-2 bg-gray-50 rounded-md">
             <img src={cardInfo ? cardInfo.img : ""} className="w-30 h-30" />
             <span>{name}</span>
         </div>
