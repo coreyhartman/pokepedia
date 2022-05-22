d776df3ff3c734901eba8cb87c3ee6027932cda9
Corey Hartman <corey.hartman1420@gmail.com>
2022-05-22 13:00:30 -0400

    Updated grid card picture and styling.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -6,8 +6,8 @@ export default function PokemonCard({ name }) {
     const pokemon = pokemons.find(x => x.name === name);
 
     return (
-        <div className="p-4 flex flex-col items-center space-y-2 bg-gray-50 rounded-md">
-            <img src={pokemon.sprites.front_default} className="w-30 h-30" />
+        <div className="p-4 flex flex-col items-center space-y-3 bg-gray-50 rounded-md border border-gray-300">
+            <img src={pokemon.sprites.other.home.front_default} className="w-30 h-30" />
             <span>{pokemon.name}</span>
         </div>
     )
