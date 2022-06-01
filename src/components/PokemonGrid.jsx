78dc13bd6842e448f117563c2639888a04fb17b3
Corey Hartman <corey.hartman1420@gmail.com>
2022-05-31 20:40:46 -0400

    Updates to pokemon grid col based on screen size.

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -6,8 +6,8 @@ export default function PokemonGrid() {
     const { isLoading, pokemons } = useContext(AppContext);
 
     return (
-        <div className="grid grid-cols-5 gap-4 md:w-3/4 lg:w-2/3 xl:w-1/2">
-            {isLoading ? <span>Loading Pokemon...</span> : pokemons.slice(0, 20).map(x => (<PokemonCard key={x.name} name={x.name} />))}
-        </div >
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:w-3/4 md:grid-cols-4 lg:w-2/3 lg:grid-cols-5 xl:w-1/2 xl:grid-cols-6">
+            {isLoading ? <span>Loading Pokemon...</span> : pokemons.slice(0, 24).map(x => (<PokemonCard key={x.name} name={x.name} />))}
+        </div>
     );
 }
